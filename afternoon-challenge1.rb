816dcb73d96cce1f70acc5c8ac17356ecd493749
LuisaZ <luisaziccarelli@gmail.com>
2019-11-05 09:36:52 +1100

    initial commit

---
+++ afternoon-challenge1.rb
@@ -0,0 +1,13 @@
+
+puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}! How old are you?"
+age = gets.chomp.to_i
+puts "Wow! You are #{age} years old. Congratulations!" 
+puts "#{name}, in #{100 - age} years you will be 100!"
+
+
+
+
+
+
