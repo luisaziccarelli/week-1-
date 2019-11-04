d90f2761fbce133a9bb91d439f759f7d0e8a3aa7
LuisaZ <luisaziccarelli@gmail.com>
2019-11-05 09:37:35 +1100

    initial commit

---
+++ morning-challenge.rb
@@ -0,0 +1,16 @@
+#Famous building challenge 
+
+eiffel_tower_height= 324 "meters"
+big_ben_height= 96 "meters"
+taj_mahal_height= 73 "meters"
+
+
+total_height= eiffel_tower_height + big_ben_height + taj_mahal_height
+
+Puts "Which #{building} would you like to learn the height of?"
+
+
+building= gets.comp 
+
+Puts total_height
+
