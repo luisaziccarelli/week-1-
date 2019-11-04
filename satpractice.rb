5e7bc3bf1ae97a80da050b318480fb69ea819393
LuisaZ <luisaziccarelli@gmail.com>
2019-11-05 09:38:08 +1100

    initial commit

---
+++ satpractice.rb
@@ -0,0 +1,17 @@
+first_name= "Luisa"
+middle_name= "Maria"
+surname= "Ziccarelli"
+space= " "
+
+
+full_name= first_name + space + middle_name + space + surname
+
+
+
+
+puts full_name 
+print full_name
+
+
+
+
