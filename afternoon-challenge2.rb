875888b941c958c763ba4860dbe9604e16f7c1bf
LuisaZ <luisaziccarelli@gmail.com>
2019-11-05 09:37:00 +1100

    initial commit

---
+++ afternoon-challenge2.rb
@@ -0,0 +1,10 @@
+#String interpolation challenge 
+
+commute_time= "1 hour"
+commute_method= "train and a bus"
+destination= "coder academy"
+
+puts "It takes me #{commute_time} to get to class at #{destination}. 
+I have to take a #{commute_method} to get there" 
+
+  
